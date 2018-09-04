1941de206da0f74886de24d951c5ed7dfe5899a5
CunjunYin <42023214+CunjunYin@users.noreply.github.com>
2018-09-04 14:49:33 +0800

    Add files via upload

--- DeviationTest/DeviationTest.java
+++ DeviationTest/DeviationTest.java
@@ -1,3 +1,5 @@
+package teamH;
+
 /**
  * 
  * @author K Ho
@@ -6,19 +8,76 @@
  * Returns true if the current temperature is considered extraordinary
  */
 public class DeviationTest {
-	public boolean checkDeviation(CSVMonitor monitor , float acceptableDeviation, int daysToAverage) {
 	
-	double tempCurrent = monitor.getMostRecentData().getTemp();
-	double tempAverage = 0;
-	double tempSum = 0;
+	/*
+	 * global variable
+	 */
+	// count how long is the data out of the deviation is in minutes 
+	private int time_out_count;
 	
-	for(int i = 0 ; i < daysToAverage ; i++) {
-		Pair historicPair;
-		historicPair = monitor.getPairFromTime(monitor.getMostRecentData().getDateTime().minusDays(i + 1));
-		tempSum += historicPair.getTemp();
+	// in a period of time, count number of times, temperature is very un-consistent
+	private int accumulated_count;
+	private int period;
+	
+	// how many days is of used for standard deviation
+	private int daysToAverage;
+	
+	public boolean checkDeviation(CSVMonitor monitor , float acceptableDeviation) {
+		double tempCurrent = monitor.getMostRecentData().getTemp();
+		double tempAverage = 0;
+		double tempSum = 0;
+		
+		for(int i = 0 ; i < daysToAverage ; i++) {
+			Pair historicPair;
+			historicPair = monitor.getPairFromTime(monitor.getMostRecentData().getDateTime().minusDays(i + 1));
+			tempSum += historicPair.getTemp();
+		}
+		
+		tempAverage = tempSum/daysToAverage;
+		return Math.abs(tempAverage - tempCurrent) >= acceptableDeviation;
 	}
 	
-	tempAverage = tempSum/daysToAverage;
-	return Math.abs(tempAverage - tempCurrent) >= acceptableDeviation;
+	public boolean checkTime_out_count() {
+		
+		return false;
+	}
+	
+	// getter and setter methods
+	public int getTime_out_count() {
+		return time_out_count;
+	}
+
+	public void setTime_out_count(int time_out_count) {
+		this.time_out_count = time_out_count;
+	}
+
+	public int getAccumulated_count() {
+		return accumulated_count;
+	}
+
+	public void setAccumulated_count(int accumulated_count) {
+		this.accumulated_count = accumulated_count;
+	}
+
+	public int getDaysToAverage() {
+		return daysToAverage;
+	}
+
+	public void setDaysToAverage(int daysToAverage) {
+		this.daysToAverage = daysToAverage;
+	}
+
+	/**
+	 * @return the period
+	 */
+	public int getPeriod() {
+		return period;
+	}
+
+	/**
+	 * @param period the period to set
+	 */
+	public void setPeriod(int period) {
+		this.period = period;
 	}
 }
