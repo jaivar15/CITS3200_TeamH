27bcceb0533b4aab3b748b6234c7c4a67228b304
Mitch <22233087@student.uwa.edu.au>
2018-10-22 12:26:37 +0800

    Add advanced alerts Make deviationCheck changes Add getting sender email and pass

--- parser/CSVMonitor.java
+++ parser/CSVMonitor.java
@@ -3,6 +3,8 @@ package parser;
 import DeviationTest.DataPoint;
 import DeviationTest.AnimalDataSet;
 import GUI.swing.Animal;
+import GUI.swing.Home;
+import GUI.swing.Notifications;
 import GUI.swing.responder;
 import java.io.*;
 import java.time.LocalDateTime;
@@ -16,41 +18,47 @@ import javax.mail.internet.MimeMultipart;
 
 import static DeviationTest.DeviationChecks.checkDeviationLatest;
 
-public class CSVMonitor extends Thread implements Serializable{
+public class CSVMonitor extends Thread{
 	
 	private int charCount, lineCount, consecutiveTimeForAlert, daysToCheck; //get someone to make consec. time minutes or
-	private double deviationBeforeAlert;									//hours and define this, make it an int in settings.
+	private double[] deviationBeforeAlert;									//hours and define this, make it an int in settings.
 	private Animal animal;
 	private LocalDateTime timeToAlert;
 	private BufferedReader reader;
 	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 	private String delimiter, sensorID, senderEmail, senderEmailPass;
-	private String[] receivingEmails;
+	private String[] receivingEmails = new String[100];
 	private AnimalDataSet data;
 	private AtomicBoolean stop = new AtomicBoolean(false);
 	private Boolean initialRead = true;
-	private double[] alertLevels = new double[2];
 	private boolean advancedAlerts = false;
 	private LocalDateTime lastDataPointReceived, lastAlertSentAt;
+	private Home home;
 
 	/**
 	 * Creating this object will monitor an animal's CSV data file and send an email when deviation parameters met
 	 * Maybe this one for backup feature?
 	 * @param a Animal that this monitor object will be watching
 	 */
-	public CSVMonitor(Animal a, int startLine) {
+	public CSVMonitor(Animal a, Home h, int startLine) {
+		home = h;
 		lastAlertSentAt = null;
 		animal = a;
 		consecutiveTimeForAlert = (int) animal.getDuration();
-		deviationBeforeAlert = animal.getDeviation();
 		daysToCheck = animal.getDays();
-		//TODO
-		//advancedAlerts = animal.getAdvancedAlerts();
+		advancedAlerts = animal.getAdvancedAlerts();
 		if(advancedAlerts){
-			alertLevels[0] = animal.getOrangeDev();
-			alertLevels[1] = animal.getRedDev();
+			deviationBeforeAlert = new double[3];
+			deviationBeforeAlert[0] = animal.getDeviation();
+			deviationBeforeAlert[1] = animal.getOrangeDev();
+			deviationBeforeAlert[2] = animal.getRedDev();
+		} else {
+			deviationBeforeAlert = new double[1];
+			deviationBeforeAlert[0] = animal.getDeviation();
 		}
-		receivingEmails = responder.getEmailsForAnimal(animal.getAnimalName()).toArray(new String[0]);
+		if (home.getResponder().getEmailsForAnimal(animal.getAnimalName()) != null){
+			receivingEmails = home.getResponder().getEmailsForAnimal(animal.getAnimalName()).toArray(new String[0]);
+		}
 		charCount = 0;
 		lineCount = 0;
 		delimiter = ",";
@@ -77,8 +85,8 @@ public class CSVMonitor extends Thread implements Serializable{
 	 * Use this one for now
 	 * @param a Animal that this monitor object will be watching
 	 */
-	public CSVMonitor(Animal a){
-		this(a, 0);
+	public CSVMonitor(Animal a, Home h){
+		this(a, h, 0);
 	}
 	
 	/**
@@ -89,15 +97,20 @@ public class CSVMonitor extends Thread implements Serializable{
 		if (!animal.equals(temp)){
 			animal = temp;
 			consecutiveTimeForAlert = (int) animal.getDuration();
-			deviationBeforeAlert = animal.getDeviation();
 			daysToCheck = animal.getDays();
-			//TODO
-			//advancedAlerts = animal.getAdvancedAlerts();
+			advancedAlerts = animal.getAdvancedAlerts();
 			if(advancedAlerts){
-				alertLevels[0] = animal.getOrangeDev();
-				alertLevels[1] = animal.getRedDev();
+				deviationBeforeAlert = new double[3];
+				deviationBeforeAlert[0] = animal.getDeviation();
+				deviationBeforeAlert[1] = animal.getOrangeDev();
+				deviationBeforeAlert[2] = animal.getRedDev();
+			} else {
+				deviationBeforeAlert = new double[1];
+				deviationBeforeAlert[0] = animal.getDeviation();
 			}
-			receivingEmails = responder.getEmailsForAnimal(animal.getAnimalName()).toArray(new String[0]);
+			if (home.getResponder().getEmailsForAnimal(animal.getAnimalName()) != null){
+				receivingEmails = home.getResponder().getEmailsForAnimal(animal.getAnimalName()).toArray(new String[0]);
+			}
 		}
 	}
 	
@@ -185,7 +198,7 @@ public class CSVMonitor extends Thread implements Serializable{
 							timeToAlert = data.getLatestUpdate().getTime().plusHours(consecutiveTimeForAlert);
 						} else if (timeToAlert.compareTo(latest.getTime()) <= 0 ){
 							lastAlertSentAt = latest.getTime();
-							sendMail(check[1]);
+							sendMail((int) check[1]);
 						}
 					}
 					if (timeToAlert != null && check[0] == 0){
@@ -218,21 +231,26 @@ public class CSVMonitor extends Thread implements Serializable{
 	/**
 	 * Sends mail
 	 * TODO: Add receiving emails list to this, add way to get sender email and pass.
-	 * TODO: What message to send also.
 	 */
-	private void sendMail(double deviation){
+	private void sendMail(int alertL){
 		mail mailer = null;
-		String alertLevel = "ALERT";
-		//senderEmail = getSenderEmail();
-		//senderEmailPass = getSenderEmailPass();
+		String alertLevel = "";
+		senderEmail = home.getResponder().getSenderEmail();
+		senderEmailPass = home.getResponder.getSenderEmailPass();
 		if (advancedAlerts){
-			alertLevel = "Yellow Alert";
-			if (deviation > alertLevels[1]){
-				alertLevel = "Orange Alert";
+			switch (alertL) {
+				case (1):
+					alertLevel = "Green";
+					break;
+				case (2):
+					alertLevel = "Orange";
+					break;
+				case (3):
+					alertLevel = "Red";
+					break;
 			}
-			if (deviation > alertLevels[2]){
-				alertLevel = "Red Alert";
-			}
+		} else {
+			alertLevel = "ALERT";
 		}
 		try {
 			mailer = new mail(senderEmail, senderEmailPass, "587", receivingEmails);
@@ -243,84 +261,16 @@ public class CSVMonitor extends Thread implements Serializable{
 		mailer.initSmtpPort(props);
 		MimeMultipart finals;
 		try {
-			finals = mailer.text(alertLevel + ": " + animal.getAnimalName() + "'s temperature has deviated by " + deviation);
+			finals = mailer.text(alertLevel + ": " + animal.getAnimalName() + "'s temperature has deviated");
 			mailer.sent(props,finals);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		//home.addNotification(new Notifications(animal.getAnimalName(), animal.getAnimalDescription(), alertLevel));
 	}
 	
 	public Animal getMonitoredAnimal() {
 		return animal;
 	}
-	
-	//Legacy Code
-	/*
-	public List<Pair> getMostRecentData(int listLength) {
-		return Data.subList(Data.size() - listLength, Data.size());
-	}
-	
-	//////////////////////////////////////////////////////////////////
-	
-	public Pair getPairFromTime(LocalDateTime soughtTime) {
-	
-		//iterate through list looking for a specific time (assuming chronological entry)
-		for(int i = 0 ; i < Data.size(); i ++) {
-			
-			//if the current checked time is before what you are looking for, keep searching
-		if(Data.get(i).getDateTime().isBefore(soughtTime)) {
-			continue;
-		}
-		
-		//if the current checked time is what you are looking for, return it
-		else if(Data.get(i).getDateTime().isEqual(soughtTime)) {
-			return Data.get(i);
-		}
-		
-		//if the current checked and the previously checked times "flank" the time you are looking for, return an averaged temperature pair
-		else if(Data.get(i - 1).getDateTime().isBefore(soughtTime) && Data.get(i).getDateTime().isAfter(soughtTime)) {
-			return new Pair(soughtTime, (Data.get(i-1).getTemp() + Data.get(i).getTemp())/2);
-		}
-	}
-	
-		//if the time did not work
-	return null;
-	}
-	
-	/////////////////////////////////////////////////////////////////////
-	
-	
-}
 
-class Pair {
-	private LocalDateTime dateTime;
-	private double temp;
-	private String dateFormat = "dd/MM/yyyy HH:mm";
-	Pair(LocalDateTime dt, double temp){
-		dateTime = dt;
-		this.temp = temp;
-	}
-	
-	public LocalDateTime getDateTime(){
-		return dateTime;
-	}
-	
-	public String getDateTimeString(){
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
-		return formatter.format(dateTime);
-	}
-	
-	public double getTemp(){
-		return temp;
-	}
-	
-	@Override
-	public String toString() {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
-		String str = formatter.format(dateTime) + ", " + String.format("%.3f", temp);
-		return str;
-	}
-}
-
-*/
 }
